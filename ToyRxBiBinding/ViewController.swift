a979b24fe6b27d5103e8bcb9d5a88bbd0cf32e26
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-11-04 22:07:30 +0800

    Initial Commit

---
+++ ToyRxBiBinding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToyRxBiBinding
+//
+//  Created by Faiz Mokhtar AD0502 on 04/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
