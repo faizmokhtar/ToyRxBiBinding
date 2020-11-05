aed0e971644b6290204331c1ef8a7b0b864851ea
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-11-05 09:51:08 +0800

    try two way binding

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ToyRxBiBinding' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ToyRxBiBinding
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'RxBiBinding'
+
+end
--- ToyRxBiBinding/ViewController.swift
+++ ToyRxBiBinding/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxBiBinding
 
 class ViewController: UIViewController {
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    
+    let bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        (firstTextField.rx.text <-> secondTextField.rx.text)
+            .disposed(by: bag)
     }
 
 
